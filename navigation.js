6ce8cbcf2a9b7b4ea0778fec54e15c0f4d61ecc4
loganfreeman <shanhongcheng@hotmail.com>
2016-10-19 11:11:30 -0600

    Create navigation.js

---
+++ navigation.js
@@ -0,0 +1,9 @@
+	blockNavigation.on('click', 'button', function(){
+		var direction = $(this),
+			indexVisibleblock = imagesList.index(imageWrapper.children('li.is-selected'));
+
+		if( !direction.hasClass('inactive') ) {
+			var index = ( direction.hasClass('cd-next') ) ? (indexVisibleblock + 1) : (indexVisibleblock - 1); 
+			updateBlock(index);
+		}
+	});
