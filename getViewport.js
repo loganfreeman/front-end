2e1f7e8f288c715597b8c9119901c2d8c3592102
loganfreeman <shanhongcheng@hotmail.com>
2016-10-26 16:29:34 -0600

    Create getViewport.js

---
+++ getViewport.js
@@ -0,0 +1,17 @@
+	/**
+	 * gets the viewport width and height
+	 * based on http://responsejs.com/labs/dimensions/
+	 */
+	function getViewport( axis ) {
+		var client, inner;
+		if( axis === 'x' ) {
+			client = docElem['clientWidth'];
+			inner = window['innerWidth'];
+		}
+		else if( axis === 'y' ) {
+			client = docElem['clientHeight'];
+			inner = window['innerHeight'];
+		}
+		
+		return client < inner ? inner : client;
+	}
