abe68e4cc1144828af4463e24860e1a0b109c93d
loganfreeman <shanhongcheng@hotmail.com>
2016-10-29 05:18:13 -0600

    Update webkitTransitionEnd.js

--- webkitTransitionEnd.js
+++ webkitTransitionEnd.js
@@ -1,5 +1,5 @@
-				$(this).addClass('open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-					//wait for the end of the transition and set the animating variable to tru
-					self.animating = false;
-					$(this).off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-				});
+$(this).addClass('open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+	//wait for the end of the transition and set the animating variable to tru
+	self.animating = false;
+	$(this).off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+});
