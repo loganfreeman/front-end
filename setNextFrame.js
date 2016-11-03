c262fe04aa3c9a6269f3088df5a987c269034133
loganfreeman <shanhongcheng@hotmail.com>
2016-11-03 10:57:00 -0600

    Create setNextFrame.js

---
+++ setNextFrame.js
@@ -0,0 +1,6 @@
+var raf = (typeof window !== 'undefined' && window.requestAnimationFrame) || setTimeout;
+var nextFrame = function(fn) { raf(function() { raf(fn); }); };
+
+function setNextFrame(obj, prop, val) {
+  nextFrame(function() { obj[prop] = val; });
+}
