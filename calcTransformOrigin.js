b9a0a4d3794e128311f0b21193b7a5ce808e001c
loganfreeman <shanhongcheng@hotmail.com>
2016-11-03 10:58:40 -0600

    Create calcTransformOrigin.js

---
+++ calcTransformOrigin.js
@@ -0,0 +1,11 @@
+function calcTransformOrigin(isTextNode, textRect, boundingRect) {
+  if (isTextNode) {
+    if (textRect) {
+      //calculate pixels to center of text from left edge of bounding box
+      var relativeCenterX = textRect.left + textRect.width/2 - boundingRect.left;
+      var relativeCenterY = textRect.top + textRect.height/2 - boundingRect.top;
+      return relativeCenterX + 'px ' + relativeCenterY + 'px';
+    }
+  }
+  return '0 0'; //top left
+}
