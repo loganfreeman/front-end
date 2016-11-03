803a51d38e37222fbe715967a0c740666a527fb0
loganfreeman <shanhongcheng@hotmail.com>
2016-11-03 10:57:52 -0600

    Create getTextNodeRect.js

---
+++ getTextNodeRect.js
@@ -0,0 +1,11 @@
+function getTextNodeRect(textNode) {
+  var rect;
+  if (document.createRange) {
+    var range = document.createRange();
+    range.selectNodeContents(textNode);
+    if (range.getBoundingClientRect) {
+        rect = range.getBoundingClientRect();
+    }
+  }
+  return rect;
+}
